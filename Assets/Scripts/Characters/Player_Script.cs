73e7f9263619fdb492d2773d1cde41c6332a737e
SushrutDhakal <101437459+SushrutDhakal@users.noreply.github.com>
2022-12-17 16:24:52 -0500

    hehe

--- Assets/Scripts/Characters/Player_Script.cs
+++ Assets/Scripts/Characters/Player_Script.cs
@@ -6,17 +6,17 @@ public class Player_Script : MonoBehaviour
 {
     public float movementSpeed = 5f;
     public Rigidbody2D myrigidbody;
-    //public SpriteRenderer mysprite;
-    //public Animator myanimation;
+    public SpriteRenderer mysprite;
+    public Animator myanimation;
     Vector2 movement;
     Vector3 forward;
     Vector3 backword;
-
+   
     void Start()
     {
         myrigidbody.freezeRotation = true;
     }
-
+        
 
     // Update is called once per frame
     void Update()
@@ -25,11 +25,24 @@ public class Player_Script : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        myanimation.SetFloat("Vertical", movement.y);
+        myanimation.SetFloat("Horizontal", Mathf.Abs(movement.x));
+
+      
     }
     void FixedUpdate()
     {
-
+        
+        if (movement.x > 0)
+        {
+            mysprite.flipX = false;
+        }
+        if (movement.x  < 0)
+        {
+            mysprite.flipX = true;
+        }
         myrigidbody.velocity = movement * movementSpeed;
+       
 
     }
 }
