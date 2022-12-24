6c28d9bed2f2865d6b3f32d9496b47cd0b19786b
SushrutDhakal <101437459+SushrutDhakal@users.noreply.github.com>
2022-12-24 12:51:54 -0500

    flicker light

--- Assets/Scripts/LevelOne/Characters/Enemy/Enemy_hide.cs
+++ Assets/Scripts/LevelOne/Characters/Enemy/Enemy_hide.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy_hide : MonoBehaviour
 {
-
+    public UnityEngine.Rendering.Universal.Light2D light2D;
     public SpriteRenderer mysprite;
     public Countdown countdown;
     public Transform tf;
@@ -14,30 +14,34 @@ public class Enemy_hide : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bc.enabled = false; //hide enemy on start 
+        bc.enabled = false;
         mysprite.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        showEnemy();
+        //Debug.Log(timeStart);
+        timeOver();
     }
 
-    void showEnemy() 
+    void timeOver()
     {
         TimeStart = Mathf.RoundToInt(countdown.timeStart);
 
-        //if the rounded time is 0, then show the enemy and transform to that position
+        Debug.Log(countdown.timeStart);
+
+        if (countdown.timeStart > 0 && countdown.timeStart < 1)
+        {
+            light2D.intensity = 1f;
+        }
 
         if (TimeStart == 0)
         {
+            light2D.intensity = 0.15f;
             bc.enabled = true;
             mysprite.enabled = true;
             tf.position = new Vector3(7.76f, 3.5f, 0);
         }
     }
 }
-
-    
-
