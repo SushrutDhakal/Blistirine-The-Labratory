67d0e324fdb10873603f1d4d0acee932ca5b13c0
SushrutDhakal <101437459+SushrutDhakal@users.noreply.github.com>
2022-12-28 18:12:05 -0500

    smoke bomb + use

--- Assets/Scripts/LevelOne/Inventory Scripts/Use/UseSmoke.cs
+++ Assets/Scripts/LevelOne/Inventory Scripts/Use/UseSmoke.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class UseSmoke : MonoBehaviour
 {
+
+    public GameObject effect;
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Use()
     {
-        
+        Instantiate(effect, player.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
