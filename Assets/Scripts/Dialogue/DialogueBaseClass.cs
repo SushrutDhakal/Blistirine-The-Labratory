37715362288398b7f64c51229205541c3323af10
SushrutDhakal <101437459+SushrutDhakal@users.noreply.github.com>
2022-12-28 22:16:35 -0500

    minor nothing

---
+++ Assets/Scripts/Dialogue/DialogueBaseClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueBaseClass : MonoBehaviour
+    {
+        protected IEnumerator WriteText(string input, Text textHolder, Color textColor, Font textFont, float delay)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                textHolder.text += input[i];
+                yield return new WaitForSeconds(delay);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+
+        [Header("Text Options")]
+        [SerializeField] private string input;
+        [SerializeField] private Color textColor;
+        [SerializeField] private Font textFont;
+
+        [Header("Time parameters")]
+        [SerializeField] private float delay;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+            StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay));
+
+        }
+
+
+    }
+}
