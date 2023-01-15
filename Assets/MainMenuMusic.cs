ba7eafd70d96f6dbfaf7395f41a2a6076c28b419
MMShed <maheermshed@gmail.com>
2023-01-15 11:52:40 -0500

    Soundtrack Update

---
+++ Assets/MainMenuMusic.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    public AudioSource source;
+    
+
+    public void Update ()
+    {
+        source.Play();
+    }
+
+}
